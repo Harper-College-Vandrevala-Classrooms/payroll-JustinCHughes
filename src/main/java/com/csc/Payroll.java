f26f4bcf90b87b171355cb87c71904e9d1bbadd1
JustinCHughes <hcjustin713@gmail.com>
2024-09-03 23:05:08 -0500

    Finished base functionality of program. Finished life insurance question and usage. Needs portfolio credit 1, 1, and 2. Work in progress.

--- src/main/java/com/csc/Payroll.java
+++ src/main/java/com/csc/Payroll.java
@@ -5,12 +5,14 @@ public class Payroll {
   // Implement your solution here!
 
   private static float UNION_FEES = (float) 10.00;
+  private static float PAY_RATE = (float) 16.78;
 
   public static void main(String[] args) {
         
     System.out.print("Welcome to the Payroll Program!\n\n");
     float hours;
     int dependents;
+    int insurance;
     int lifeInsurance;
     float pay;
     float net;
@@ -19,94 +21,119 @@ public class Payroll {
                     , "\nInput was invalid. Please input a positive number.\n");
     dependents = checkInt("How many children do you have? "
                     , "\nInput was invalid. Please input a positive number.\n");
+
+    if (dependents < 3)
+    {
+      insurance = 15;
+    }
+    else
+    {
+      insurance = 35;
+    }
+
     lifeInsurance = lifeInsuranceSelection(dependents);
 
     if (hours > 40)
     {
-      pay = (float) 16.78 * 40;
-      pay = pay + ((hours - 40) * (float) 1.5 * (float) 16.78);
+      pay = (float) PAY_RATE * 40;
+      pay = pay + ((hours - 40) * (float) 1.5 * (float) PAY_RATE);
     }
     else
     {
-      pay = hours * (float) 16.78;
+      pay = hours * (float) PAY_RATE;
     }
 
-    System.out.print("Payroll Stub:\n");
-    System.out.print("Hours:   " + String.format("%.2f", hours));
-    System.out.print("Rate:   16.75 $/hr");
-    System.out.print("Gross:   $ " + String.format("%.2f", pay));
+    System.out.print("\nPayroll Stub:");
+    System.out.print("\n\nHours:   " + String.format("%.2f", hours));
+    System.out.print("\nRate:   16.75 $/hr");
+    System.out.print("\nGross:   $ " + String.format("%.2f", pay));
 
-    System.out.print("\nSocSec:   $ " + String.format("%.2f", (pay * (float) 0.06)));
-    System.out.print("FedTax:   $ " + String.format("%.2f", (pay * (float) 0.14)));
-    System.out.print("StTax:   $ " + String.format("%.2f", (pay * (float) 0.05)));
-    System.out.print("Union:   $ 10.00");
-    System.out.print("Ins:   $ " + ); // Needs logic
-    System.out.print("LifeIns:   $ " + String.format(".2f", lifeInsurance));
+    System.out.print("\n\nSocSec:   $ " + String.format("%.2f", (pay * (float) 0.06)));
+    System.out.print("\nFedTax:   $ " + String.format("%.2f", (pay * (float) 0.14)));
+    System.out.print("\nStTax:   $ " + String.format("%.2f", (pay * (float) 0.05)));
+    System.out.print("\nUnion:   $ 10.00");
+    System.out.print("\nIns:   $ " + String.format("%.2f", (float) insurance)); // Needs logic
+    System.out.print("\nLifeIns:   $ " + String.format("%.2f", (float) lifeInsurance));
 
-    net = pay - (pay * (float) .25) - UNION_FEES - lifeInsurance;
+    net = pay - (pay * (float) .25) - UNION_FEES - insurance - lifeInsurance;
 
-    System.out.print("\nNet:   $ " + String.format("%.2f", net));
+    System.out.print("\nNet:   $ " + String.format("%.2f", net));
   }
 
   public static int checkInt(String checkString, String errorString)
   {
-    Scanner in = new Scanner(System.in);
-    boolean check = false;
+    Scanner checkIntScanner = new Scanner(System.in);
     int userInput = 0;
 
-    do { 
+    do {
       System.out.print(checkString);
-      if (in.hasNextInt())
+      if (checkIntScanner.hasNextInt())
       {
-        userInput = in.nextInt();
-
+        userInput = checkIntScanner.nextInt();
         if(userInput >= 0)
         {
-          check = true;
+          break;
+        }
+        else
+        {
+          System.out.print(errorString);
+          if(checkIntScanner.hasNextLine())
+          {
+            checkIntScanner.nextLine();
+          }
         }
       }
       else
       {
         System.out.print(errorString);
+        if(checkIntScanner.hasNextLine())
+        {
+          checkIntScanner.nextLine();
+        }
       }
-    } while (check);
+    } while (true);
 
-    in.close();
     return userInput;
   }
 
   public static float checkFloat(String checkString, String errorString)
   {
-    Scanner in = new Scanner(System.in);
-    boolean check = false;
+    Scanner checkFloatScanner = new Scanner(System.in);
     float userInput = 0;
 
     do { 
       System.out.print(checkString);
-      if (in.hasNextFloat())
+      if (checkFloatScanner.hasNextFloat())
       {
-        userInput = in.nextFloat();
-
+        userInput = checkFloatScanner.nextFloat();
         if(userInput >= 0)
         {
-          check = true;
+          break;
+        }
+        else
+        {
+          System.out.print(errorString);
+          if(checkFloatScanner.hasNextLine())
+          {
+            checkFloatScanner.nextLine();
+          }
         }
       }
       else
       {
         System.out.print(errorString);
+        if(checkFloatScanner.hasNextLine())
+        {
+          checkFloatScanner.nextLine();
+        }
       }
-    } while (check);
+    } while (true);
 
-    in.close();
     return userInput;
   }
 
   public static int lifeInsuranceSelection(int dependents)
   {
-    Scanner in = new Scanner(System.in);
-    boolean check = false;
-
     int userInput = 0;
 
     do { 
@@ -120,7 +147,7 @@ public class Payroll {
                            ""","\nInvalid input. Input must be '1','2', '3' or '4'.\n");
       if(userInput > 0 && userInput <= 3)
       {
-        check = true;
+        break;
       }
       else if(userInput == 4)
       {
@@ -132,16 +159,14 @@ public class Payroll {
         }
         else
         {
-          check = true;
+          break;
         }
       }
       else
       {
         System.out.print("\nInvalid input. Input must be '1','2', '3' or '4'.\n");
       }
-    } while (check);
-
-    in.close();
+    } while (true);
 
     switch (userInput)
     {
