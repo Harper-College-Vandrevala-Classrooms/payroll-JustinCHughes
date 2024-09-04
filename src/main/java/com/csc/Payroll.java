541abcf758eb20c06d89124d9c2955b48b25311e
JustinCHughes <hcjustin713@gmail.com>
2024-09-04 05:08:55 -0500

    Added comments

--- src/main/java/com/csc/Payroll.java
+++ src/main/java/com/csc/Payroll.java
@@ -8,7 +8,8 @@ public class Payroll {
   private static float PAY_RATE = (float) 16.78;
 
   public static void main(String[] args) {
-        
+    
+    // Welcome user and initialize variables
     System.out.print("Welcome to the Payroll Program!\n\n");
     float hours;
     int dependents;
@@ -17,11 +18,15 @@ public class Payroll {
     float pay;
     float net;
 
+    // Calls checkFloat to ensure hours input is a positive float
     hours = checkFloat("How many hours did you work this week? "
                     , "\nInput was invalid. Please input a positive number.\n");
+
+    // Calls checkInt to make sure user input a positive integer
     dependents = checkInt("How many children do you have? "
                     , "\nInput was invalid. Please input a positive number.\n");
 
+    // Determines how much worker owes in insurance depending on dependents
     if (dependents < 3)
     {
       insurance = 15;
@@ -31,8 +36,10 @@ public class Payroll {
       insurance = 35;
     }
 
+    // Calls lifeInsurance function for user to choose lifeInsurance amount
     lifeInsurance = lifeInsuranceSelection(dependents);
 
+    // Calculates pay and overtime pay
     if (hours > 40)
     {
       pay = (float) PAY_RATE * 40;
@@ -43,6 +50,7 @@ public class Payroll {
       pay = hours * (float) PAY_RATE;
     }
 
+    // Prints out function's calculated paystub
     System.out.print("\nPayroll Stub:");
     System.out.print("\n\nHours:   " + String.format("%.2f", hours));
     System.out.print("\nRate:   16.75 $/hr");
@@ -53,7 +61,7 @@ public class Payroll {
     System.out.print("\nStTax:   $ " + String.format("%.2f", (pay * (float) 0.05)));
     System.out.print("\nUnion:   $ 10.00");
     System.out.print("\nIns:   $ " + String.format("%.2f", (float) insurance)); // Needs logic
-    System.out.print("\nLifeIns:   $ " + String.format("%.2f", (float) lifeInsurance));
+    System.out.print("\nLifeIns:   $ " + String.format("%.2f", (float) lifeInsurance) + "\n");
 
     net = pay - (pay * (float) .25) - UNION_FEES - insurance - lifeInsurance;
 
@@ -65,6 +73,9 @@ public class Payroll {
     Scanner checkIntScanner = new Scanner(System.in);
     int userInput = 0;
 
+    // Prompts user using input string for an integer
+    // Continues to run until user input an int of 0 or greater
+    // Breaks out of loop once input meets parameters
     do {
       System.out.print(checkString);
       if (checkIntScanner.hasNextInt())
@@ -101,6 +112,9 @@ public class Payroll {
     Scanner checkFloatScanner = new Scanner(System.in);
     float userInput = 0;
 
+    // Prompts user using input string for a float
+    // Continues to run until user input a float of 0 or greater
+    // Breaks out of loop once input meets parameters
     do { 
       System.out.print(checkString);
       if (checkFloatScanner.hasNextFloat())
@@ -137,6 +151,11 @@ public class Payroll {
     int userInput = 0;
 
     do { 
+
+      // Calls checkInt to prompt for life insurance plan selection
+      // Notifies user on error that they must select 1, 2, 3 or 4
+      // Continues to run do while loop until user makes selection from list
+      // Also continues loop if user selects plan 4 with no children
       userInput = checkInt("""
                            Which life insurance plan do you want to select?
                            
@@ -168,6 +187,7 @@ public class Payroll {
       }
     } while (true);
 
+    // Selects life insurance cost depending on inputs value
     switch (userInput)
     {
       case 1:
